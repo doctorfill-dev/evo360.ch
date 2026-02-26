01bc1546a36e62a4bde457091f66fa46c3c91c1f
cutiips <jonathan@curty.link>
2026-02-26 18:08:28 +0100

    fix#1: remove @nuxthq/studio module from dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxthq/studio'],
+  modules: ['@nuxt/content'],
   srcDir: 'app/',
   css: ['~/assets/css/main.scss'],
   ssr: true,
